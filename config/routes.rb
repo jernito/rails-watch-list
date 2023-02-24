cd3c821b59c329964dab23b8b791863531a55a94
jernito naisso <jernito.naisso@gmail.com>
2023-02-24 16:22:23 +0100

    premier site en ligne

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "lists#index"
-  resources :lists, only: %i[create new show] do
+  root to: "lists#index"
+  resources :lists, only: %i[create new show index] do
     resources :bookmarks, only: %i[create new]
 
     end
